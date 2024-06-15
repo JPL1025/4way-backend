c268d454152aa638bbd9d72afaf3757b183bca93
JPL1025 <79537266+JPL1025@users.noreply.github.com>
2024-06-14 19:44:48 -0500

    Testing Access settings

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ func jsonHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	json.NewEncoder(w).Encode(response)
 }
 
